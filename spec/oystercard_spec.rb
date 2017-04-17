26a38be2d7e09c3ed25b8d3840108416d77ae6a8
Filipe Fasolin <ffasolin@gmail.com>
2017-04-17 22:53:38 +0100

    Adds oystercard class and initializes spec

---
+++ spec/oystercard_spec.rb
@@ -0,0 +1,10 @@
+require 'oystercard'
+
+describe Oystercard do
+
+  it { is_expected.to respond_to(:balance) }
+  
+end
+
+#/Users/Fasolin/Projects/oystercard/spec/oystercard_spec.rb:1
+#uninitialized constant
