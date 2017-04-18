538619121fbae52f92a3c65bf126effe9219977b
Filipe Fasolin <ffasolin@gmail.com>
2017-04-18 11:44:11 +0100

    Adds top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,7 +3,16 @@ require 'oystercard'
 describe Oystercard do
 
   it { is_expected.to respond_to(:balance) }
-  
+
+  describe '#top_up' do
+    it 'should allow user to #top_up' do
+      expect(subject.top_up(0)).to eq 0
+    end
+
+    it 'should allow user to #top_up' do
+      expect(subject.top_up(91093)).to eq 91093
+    end
+  end
 end
 
 #/Users/Fasolin/Projects/oystercard/spec/oystercard_spec.rb:1
